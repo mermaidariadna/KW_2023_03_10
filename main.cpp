34fc497d2bc55ee87c52524dc38cf8380349332e
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-03-14 21:26:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 int main(int argc, char** argv){
-        if (argc == 0) std::cout << "no file" << std::endl;
+        if (argc == 1) std::cout << "no file" << std::endl;
         FILE* input = fopen(argv[1], "r");
         polindromToFile(input);
         fclose(input);
