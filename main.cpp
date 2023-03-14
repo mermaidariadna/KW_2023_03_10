d35e8b679c65acc0f372a95b023c1bfdc93af27b
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-03-14 21:30:12 +0300

    if no file

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include <fstream>
 #include <string>
 int main(int argc, char** argv){
-        if (argc == 1) std::cout << "no file" << std::endl;
+        if (argc == 1) {
+                std::cout << "no file" << std::endl;
+                return 0;
+        }
         FILE* input = fopen(argv[1], "r");
         polindromToFile(input);
         fclose(input);
         return 0;
 }
+
+
 
